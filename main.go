a2e035917da30ae86981661c85a4faedd7eef023
Josh <jdhurst@users.noreply.github.com>
2019-10-21 17:02:07 -0400

    Update main.go

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func Greet(name string) string {
 }
 
 func main() {
-	greeting := Greet("Charles")
+	greeting := Greet("Brian")
 	log.Printf(greeting)
 	for {
 		// Don't exit, otherwise Kubernetes thinks we crashed.
